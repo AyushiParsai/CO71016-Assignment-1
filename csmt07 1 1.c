ff39b2617f89696bcbaedb9f4aeec8f8a0687ed2
AyushiParsai <ayushiparsai189@gmail.com>
2026-09-08 18:57:50 +0000

    Add Assignment 1 Problem 1

---
+++ csmt07 1 1.c
@@ -0,0 +1,28 @@
+/*
+Student Name: Ayushi Parsai
+Enrollment Number: csmt07
+Assignment Number and Title: Assignment 1 - Input/Output/Operators
+Problem Number: 1
+Date of Submission: 09/09/2026
+*/
+
+#include<stdio.h>
+int main()
+{
+    int s1,s2;
+    float f1,f2;
+    printf("enter first integer:");
+    scanf("%d",&s1);
+    printf("enter second integer:");
+    scanf("%d",&s2);
+    printf("enter floating point number 1:");
+    scanf("%f",&f1);
+    printf("enter floating point number 2:");
+    scanf("%f",&f2);
+    printf("given first integer = %d\n",s1);
+    printf("given second integer = %d\n",s2);
+    printf("given floating point number 1 = %f\n",f1);
+    printf("given floating point number 2 = %f\n",f2);
+    return 0;
+
+}
