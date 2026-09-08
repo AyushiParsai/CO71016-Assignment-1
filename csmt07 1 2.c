eb98d9470732603036b7efca96992d0418220d5d
AyushiParsai <ayushiparsai189@gmail.com>
2026-09-08 19:33:18 +0000

    Add assignment 1 problem 2

---
+++ csmt07 1 2.c
@@ -0,0 +1,17 @@
+/*
+Student Name: Ayushi Parsai
+Enrollment Number: csmt07
+Assignment Number and Title: Assignment 1 - Input/Output/Operators
+Problem Number: 2
+Date of Submission: 09/09/2026
+*/
+#include<stdio.h>
+int main()
+{
+    int s1,s2;
+    float f1,f2;
+    scanf("%d %d %f %f",&s1,&s2,&f1,&f2);
+    printf("given numbers: %d %d %f %f\n",s1,s2,f1,f2);
+    return 0;
+
+}
